4a837b8269400614a1595a34b14d61c4130d71ef
Subhojit0012 <subhojitroy00012@gmail.com>
2025-05-19 11:49:12 +0530

    refactor: reorganize middleware and route imports for better structure

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ const app = express();
 
 const PORT = process.env.PORT || 8001;
 
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Import routes
+import authRoute from "./src/routers/auth.route.js"
+import problemRoute from "./src/routers/problem.route.js";
+
+app.use("/api/auth", authRoute);
+app.use("/api/problem", problemRoute);
+
 app.listen(PORT, () => {
   console.log(`Server running at: ${PORT}`);
 });
